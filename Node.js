86ca8a6f6e6ca41694311b3d812f42aefccc78e1
Yourvortex12 <Yourvortex93@gmail.com>
2025-01-02 20:19:30 +0500

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,15 +1,1 @@
-// Node.js HTTP server example
 
-const http = require('http');
-
-// Create a server
-const server = http.createServer((req, res) => {
-  res.statusCode = 200; // HTTP status code
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, this is your Princess\'s server! 💖\n');
-});
-
-// Listen on port 3000
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Server running at http://127.0.0.1:3000/');
-});
