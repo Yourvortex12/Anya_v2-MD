e54d71852b2a875dea6b86c02ffe47a5278dc4ec
Yourvortex12 <Yourvortex93@gmail.com>
2025-01-02 20:32:18 +0500

    Update config.js

--- config.js
+++ config.js
@@ -16,9 +16,9 @@ module.exports = {
     /**
      * owner details and parameters
      */
-    ownername: process.env.OWNER_NUMBER || process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
-    instagramId: process.env.Insta || "3.69_pika",
+    ownername: process.env.OWNER_NUMBER || process.env.Owner_Name || "Your-Vortex",
+    ownernumber: process.env.Owner_Number || "+923478351899",
+    instagramId: process.env.Insta || "Your._.Vortex",
 
     /**
      * other details and parameters
@@ -30,7 +30,7 @@ module.exports = {
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    sessionId: process.env.SESSION_ID || "tkcZIjmued7An_A_N_Y_A_V_2_vNIEn0s",
 
     
     
